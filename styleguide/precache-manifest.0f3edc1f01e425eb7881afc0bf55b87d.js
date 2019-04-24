393066865ce976953ceb73cbe436fa6596a01291
Alvaro AVGS. Gonzalez Serrano <alvaro.gonzalez@stkproj.com>
2019-04-24 13:16:05 +0200

    Commit for deploy

---
+++ styleguide/precache-manifest.0f3edc1f01e425eb7881afc0bf55b87d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e533d09b57f42022e60",
+    "url": "build/2.7e533d09.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "6950091c1a0060f19f54",
+    "url": "build/main.6950091c.js"
+  },
+  {
+    "revision": "fb2e3f6e0ac1303074a1af8ef6f08d2f",
+    "url": "index.html"
+  },
+  {
+    "revision": "6950091c1a0060f19f54",
+    "url": "static/css/main.666aa5ad.chunk.css"
+  }
+]);
---
+++ styleguide/precache-manifest.c746e424b9db7746ee18a756553d67c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e533d09b57f42022e60",
+    "url": "build/2.7e533d09.js"
+  },
+  {
+    "revision": "feb19782ca779508500c",
+    "url": "build/bundle.feb19782.js"
+  },
+  {
+    "revision": "a3d3b3c9d351516fa48f",
+    "url": "build/main.a3d3b3c9.js"
+  },
+  {
+    "revision": "32b11f718d3f15723dc6eff73e6796fb",
+    "url": "index.html"
+  },
+  {
+    "revision": "a3d3b3c9d351516fa48f",
+    "url": "static/css/main.666aa5ad.chunk.css"
+  }
+]);
